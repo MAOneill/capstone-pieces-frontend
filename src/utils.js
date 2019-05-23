dfe691396f3a33de1d6fb2e4537e815829bae551
Margaret O'Neill <margaret@oneillfish.com>
2019-05-23 09:43:32 -0400

    building randomized array builder into utils

---
+++ src/utils.js
@@ -0,0 +1,15 @@
+function createRandomArray(n) {
+
+
+
+    // let randomPatternId =parseInt((Math.random() * patternKeyArray.length)) ; 
+    //this randomized the name of the pattern
+    //this will be important in the guess game
+    // pattern.setAttribute('id', `pattern${patternKeyArray[randomPatternId]}`);
+    // console.log("pattern id is :", randomPatternId, patternKeyArray[randomPatternId])
+    // patternKeyArray.splice((randomPatternId),1);
+
+
+}
+
+export {createRandomArray};
