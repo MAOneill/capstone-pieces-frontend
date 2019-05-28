73bf468285883b126578aec1ab75a43fab19c8aa
Margaret O'Neill <margaret@oneillfish.com>
2019-05-28 14:05:50 -0400

    fixing high score

--- src/components/DisplayUserHighScore.js
+++ src/components/DisplayUserHighScore.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function DisplayUserHighScore(props) {
     let ascore;
-    console.log("props are",props.scores[0])
-    if (props.scores) {
+    // console.log("props are",props.scores[0])
+    if (props.scores.length !== 0) {
 
         //  ascore = props.scores[0].score
          ascore = Math.max(...props.scores.map((each) => {
@@ -11,9 +11,9 @@ export default function DisplayUserHighScore(props) {
          }))
     }
     else {
-        ascore = "no score yet"
+        ascore = 0;
     }
-    console.log(ascore);
+    // console.log(ascore);
   return (
     <div>
        <h4>Your high Score is: {ascore}</h4>
